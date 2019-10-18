8106e132edc47e867e088d805001e278ff8deb48
eduardomaldonadofernandez21 <55493415+eduardomaldonadofernandez21@users.noreply.github.com>
2019-10-18 09:44:21 +0100

    Update ejer_4.ads

--- ejer_4.ads
+++ ejer_4.ads
@@ -1,11 +1,14 @@
 package Ejer_4 with SPARK_Mode is
 
    type Vector is array (Integer range <>) of Integer;
+   result : boolean;
 
-   procedure Delete(vect: in out Vector; num: in Integer; result: out Boolean)
+   procedure Delete(vect: in out Vector; num: in Integer)
      with
+       Global => (Output => result),
        Depends => (result => (num, vect), vect => (num, vect)),
        Post=>(if result then
                 (for all J in vect'Range => vect(J) /= num or vect(J) = 0));
 
 end Ejer_4;
+
